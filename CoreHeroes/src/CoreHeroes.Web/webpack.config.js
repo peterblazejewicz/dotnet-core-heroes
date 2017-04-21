53a1c130f6f4a04717762f70140bb8d2aca6b3c3
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-04-21 21:59:14 +0200

    Build with Env variables support and POSTCSS scripts
    - autoprefixer
    - cssnano
    - webpack with minify
    - Production, Development settings

--- CoreHeroes/src/CoreHeroes.Web/webpack.config.js
+++ CoreHeroes/src/CoreHeroes.Web/webpack.config.js
@@ -1,22 +1,42 @@
 const webpack = require('webpack');
-const config = {
-  entry: {
-    site: './Client/js/site.js',
-    vendors: './Client/js/vendors.js'
-  },
-  output: {
-    filename: '[name].js',
-    path: __dirname + '/wwwroot/js'
-  },
-  plugins: [
-       new webpack.ProvidePlugin({
+const environment = process.env.ASPNETCORE_ENVIRONMENT || 'Production';
+
+module.exports = {
+    entry: {
+        site: './Client/js/site.js',
+        vendors: './Client/js/vendors.js'
+    },
+    output: {
+        filename: (environment === 'Development')
+            ? '[name].js'
+            : '[name].min.js',
+        path: __dirname + '/wwwroot/js'
+    },
+    plugins: [new webpack.ProvidePlugin({
             jQuery: 'jquery',
             $: 'jquery',
             jquery: 'jquery',
             Tether: 'tether',
             'window.Tether': 'tether',
             'window.Holder': 'holderjs'
-        })
-  ]
+        })]
 };
-module.exports = config
+
+if (process.env.ASPNETCORE_ENVIRONMENT !== 'Development') {
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"Production"'
+            }
+        }),
+        new webpack
+            .optimize
+            .UglifyJsPlugin({
+                sourceMap: true,
+                compress: {
+                    warnings: false
+                }
+            }),
+        new webpack.LoaderOptionsPlugin({minimize: true})
+    ])
+}
